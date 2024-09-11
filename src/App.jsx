b3a995f5d76bcbb60b85ff75804c36ee9dd082fe
Ivan Vedoya <ivan.vedoyadj@gmail.com>
2024-09-11 20:19:26 -0300

    Primera version

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Navbar } from "./components/navbar/Navbar";
+import { Main } from "./components/main/Main";
+import { Footer } from "./components/footer/Footer";
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Main />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
