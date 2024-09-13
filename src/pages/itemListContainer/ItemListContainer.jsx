0601d3f52d2c26d27afd4a9cd2a60a83cf6486c4
Ivan Vedoya <ivan.vedoyadj@gmail.com>
2024-09-13 20:19:11 -0300

    Agrego ItemListContainer y corrijo nombres

--- src/pages/itemListContainer/ItemListContainer.jsx
+++ src/pages/itemListContainer/ItemListContainer.jsx
@@ -1,5 +1,18 @@
-const ItemListContainer = () => {
-    return <div>itemListContainer</div>;
+import PropTypes from 'prop-types';
+import { Container, Alert } from 'react-bootstrap';
+
+const ItemListContainer = ({ greeting }) => {
+    return (
+        <Container className="mt-4">
+        <Alert variant="info">
+        <h2>{greeting}</h2>
+        </Alert>
+        </Container>
+    );
+};
+
+ItemListContainer.propTypes = {
+    greeting: PropTypes.string.isRequired,
 };
 
 export default ItemListContainer;
