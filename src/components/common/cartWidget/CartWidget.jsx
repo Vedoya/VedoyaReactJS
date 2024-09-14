700bf231e5ea15fa377f5fbf7c6acc234b8b0e54
Ivan Vedoya <ivan.vedoyadj@gmail.com>
2024-09-13 22:10:30 -0300

    Agrego Carrito y soluciono problema visual

--- src/components/common/cartWidget/CartWidget.jsx
+++ src/components/common/cartWidget/CartWidget.jsx
@@ -1,5 +1,13 @@
+import { FaShoppingCart } from 'react-icons/fa';
+import './cartwidget.css';
+
 const CartWidget = () => {
-    return <div>CartWidget</div>;
+    return (
+        <div className="cart-widget">
+        <FaShoppingCart size={24} />
+        <span className="notification">6</span>
+        </div>
+    );
 };
 
 export default CartWidget;
